084d79f9e81854db1ed0dc45e686d621b9970096
Like_Abbos <atammura.shin@gmail.com>
2023-02-09 14:07:24 +0300

    initial commit

---
+++ src/main/java/org/example/Database/DataBase.java
@@ -0,0 +1,12 @@
+package org.example.Database;
+
+import org.example.Note.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DataBase {
+    private HashMap<Integer, Note> notepad = new HashMap<>();
+    private static int id;
+}
